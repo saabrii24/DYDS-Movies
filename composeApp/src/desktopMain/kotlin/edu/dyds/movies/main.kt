0e695fee5861f8bd6229100d29670901c0dc6df3
saabrii24 <sabrinabrugniere@gmail.com>
2026-04-19 21:41:45 -0300

    Modifica la estructura del proyecto para que corresponda a la arquitectura de la etapa-1

--- composeApp/src/desktopMain/kotlin/edu/dyds/movies/main.kt
+++ composeApp/src/desktopMain/kotlin/edu/dyds/movies/main.kt
@@ -2,6 +2,7 @@ package edu.dyds.movies
 
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import edu.dyds.movies.presentation.App
 
 fun main() = application {
     Window(
